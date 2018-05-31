6c9bfe311a956d4d335aee8bb92ee55aa965c800
Nuts <nuts@saukopf.de>
2018-05-31 13:16:55 +0200

    implement lib functions

--- filenotify.py
+++ filenotify.py
@@ -9,10 +9,11 @@ Filenotify watches a directory structure and notifies users about changes in eac
 """
 
 # 17:10 - 18:10 = 1h
-# 15:00 -
+# 15:00 - 16:30 = 1.5h
 
 import os
 import sys
+import time
 import argparse
 import logging
 
@@ -31,13 +32,41 @@ class FileNotify:
         parse manifest file
 
         returns manifest dict
+        if no manifest file in dir, returns {}
+
+        manifest file format:
+        <file_base_name>;<file_date_time_string>
         """
-        pass
+
+        manifest_file = os.path.join(manifest_dir, self.manifest_file)
+        if not os.path.exists(manifest_file):
+            logger.debug("no manifest file: {}".format(manifest_file))
+            return {}
+        with open(manifest_file) as f:
+            lines = f.readlines()
+
+        manifest = {}
+        for line in lines:
+            if not line:
+                continue
+            (base, date) = line.split(';',1)
+            manifest[base]=date
+        logger.debug("read manifest from {}: {}".format(manifest_file,manifest))
+        return manifest
+
     def write_manifest(self, manifest_dir, manifest_dict):
         """
         write manifest of directory
         """
-        pass
+        # do not write if no files
+        manifest_file = os.path.join(manifest_dir, self.manifest_file)
+        if not manifest_dict:
+            return
+
+        with open(manifest_file, "w") as f:
+            for file, date in manifest_dict.items():
+                f.write("{};{}".format(file,date))
+        logger.debug("wrote manifest {}".format(manifest_file))
 
     def diff_manifest(self, old_manifest, new_manifest):
         """
@@ -48,9 +77,17 @@ class FileNotify:
         removed files are NOT mentioned
         returns {} if no changes.
         """
-        pass
+        diff = {}
+        for base_name, date in new_manifest.items():
+            if not base_name in old_manifest.keys():
+                diff[base_name]=new_manifest[base_name]
+            elif old_manifest[base_name] != new_manifest[base_name]:
+                diff[base_name]=new_manifest[base_name]
+        logger.debug("differences:{}".format(diff))
+        return diff
 
-    def create_manifest(self, manifest_dir):
+
+    def create_manifest(self, manifest_dir, manifest_files):
         """
         create manifest from directoryself
 
@@ -64,7 +101,20 @@ class FileNotify:
 
 
         """
-        pass
+        manifest = {}
+        manifest_file = os.path.join(manifest_dir, self.manifest_file)
+        logger.debug("files for new manifest: {}".format(manifest_files))
+        for file_base_name in manifest_files:
+            # ignore config and manifest
+            if file_base_name in [self.manifest_file, self.config_file]:
+                continue
+            file_name = os.path.join(manifest_dir, file_base_name)
+            timestamp = os.path.getmtime(file_name)
+            manifest[file_base_name] = time.ctime(timestamp)
+
+        logger.debug("manifest for {}: {}".format(manifest_dir, manifest))
+        return manifest
+
 
     def read_config(self,config_dir):
         """
@@ -86,20 +136,28 @@ class FileNotify:
             lines = f.readlines()
 
         for line in lines:
-            line.strip()
-            
+            line = line.strip()
             if "#" in line:
-                next
+                continue
             if not "@" in line:
-                next
+                continue
             mailaddresses.append(line)
-        pass
 
-    def notify(self, mailadresses=[], changes=[]):
+        logger.debug("read {} addresses from {}".format(len(mailaddresses), file))
+        return mailaddresses
+
+    def notify(self, root, diff_manifest):
         """
         send mail about changed files
         """
-        pass
+
+        if not diff_manifest:
+            logger.warn("no changes, should not be here")
+            return
+
+        base_dir = os.path.basename(root)
+        mailaddresses = self.read_config(root)
+        logger.info("send notifications about {} to {}".format(base_dir, mailaddresses))
 
     def run(self):
         """
@@ -114,24 +172,30 @@ class FileNotify:
             logger.debug("inside {}".format(root))
 
             # ignore dot dirs
-            if base_name.startswith('.'):
-                logger.info("ignoring directory {}".format(base_name))
-                continue
+            for dir in subdirs:
+                sub_base_name = os.path.basename(dir)
+                if sub_base_name.startswith('.'):
+                    logger.info("ignoring subdirectory {}".format(sub_base_name))
+                    subdirs.remove(dir)
 
             # look for configfile, else bail out
             if not self.config_file in files:
                 logger.info("no '{}' in '{}', ignoring directory".format(self.config_file, base_name))
+                continue
 
             # read manifest and create new manifest
             manifest = self.read_manifest(root)
-            new_manifest = self.create_manifest(root, manifest)
+            new_manifest = self.create_manifest(root, files)
 
             # compare
             diff_manifest = self.diff_manifest(manifest, new_manifest)
 
             # notify and write new manifest if there are differences
-            self.notify(root, diff_manifest)
-            self.write_manifest(root, new_manifest)
+            if diff_manifest:
+                self.notify(root, diff_manifest)
+                self.write_manifest(root, new_manifest)
+            else:
+                logger.info("nothing changed in {}".format(base_name))
 
 def cmdline(args):
     """
